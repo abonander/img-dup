da8ffb3a6a5a6657936d86d80bccf6fd430c91ce
Austin Bonander <austin.bonander@gmail.com>
2015-03-28 09:51:16 -0700

    Begin CLI refactor

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,79 +1,55 @@
-#![feature(unsafe_destructor)]
+use std::borrow::ToOwned;
+use std::env;
+use std::str::FromStr;
 
-extern crate getopts;
-extern crate image;
-extern crate img_hash;
-extern crate libc;
-extern crate serialize;
-extern crate time;
+mod dedup;
+mod load;
+mod scan;
 
-use config::{parse_args, ProgramSettings};
-use output::{output_results, test_outfile};
-use processing::process;
+#[derive(Copy)]
+enum Action {
+	Scan,
+	Dedup,
+	Load,
+}
 
-use std::io::util::NullWriter;
+impl Action {
+	fn execute<I: Iterator<Item=String>>(self, args: I) {
+		match self {
+			Scan => scan::execute(args),
+			Dedup => dedup::execute(args),
+			Load => load::execute(args),
+		}
+	}
+}
 
-use std::os;
+impl FromStr for Action {
+	type Err = String;
 
-macro_rules! json_insert(
-    ($map:expr, $key:expr, $val:expr) => (
-        $map.insert(::std::borrow::ToOwned::to_owned($key), $val.to_json())
-    );
-);
+	fn from_str(action: &str) -> Result<Self, String> {
+		let action = match action.trim() {
+			"scan" => Actions::Scan,
+			"dedup" => Actions::Dedup,
+			unk => return Err(format!("Unknown action: {:?}", unk))
+		};
 
-mod config;
-mod output;
+		Some(action)
+	}
+}
 
 fn main() {
-    let args = os::args();
-
-    let settings = parse_args(args.as_slice());
-
-	if settings.gui {
-        show_gui(settings);
-		return;
-	}
-
-    // Silence standard messages if we're outputting JSON
-    let mut out = get_output(&settings);    
-
-    match settings.outfile {
-        Some(ref outfile) => {
-            (writeln!(out, "Testing output file ({})...",
-                outfile.display())).unwrap();
-            test_outfile(outfile).unwrap();
-        },
-        None => (),        
-    };
-    
-    out.write_line("Searching for images...").unwrap();
-
-    let mut image_paths = processing::find_images(&settings);
-
-    let image_count = image_paths.len();
-
-    (writeln!(out, "Images found: {}", image_count)).unwrap();
-
-    if settings.limit > 0 {
-        (writeln!(out, "Limiting to: {}", settings.limit)).unwrap();
-        image_paths.truncate(settings.limit);
-    }
-
-    (writeln!(out, "Processing images in {} threads. Please wait...\n", 
-             settings.threads)).unwrap();
-
-    let results = processing::process(&settings, image_paths);
-
-    out.write_line("").unwrap();
-
-    output::output_results(&settings, &results).unwrap()   
+	// The first argument is the executable name
+	let mut args = env::args().skip(1);
+	
+	let action = args.next()
+		.map_or_else(
+			|arg| arg.parse::<Action>(),
+			|| Err("Please enter an action!".to_owned())
+		);
+	
+	match action {
+		Ok(action) => action.execute(args),
+		Err(msg) => io::println(msg),
+	}	
 }
-
-fn get_output(settings: &ProgramSettings) -> Box<Writer> {
-    if settings.silent_stdout() {
-        box NullWriter as Box<Writer> 
-    } else {
-        box std::io::stdio::stdout() as Box<Writer>
-    }    
-}
-
+	  
